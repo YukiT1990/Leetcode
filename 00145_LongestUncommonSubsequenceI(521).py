3c0f360e002ff94aa49710204653e3f6f9f2e83c
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-08-24 15:10:55 +0900

    modify 145

--- 00145_LongestUncommonSubsequenceI(521).py
+++ 00145_LongestUncommonSubsequenceI(521).py
@@ -1,5 +1,8 @@
 # 521. Longest Uncommon Subsequence I
 
+# Reference 
+# https://leetcode.com/problems/longest-uncommon-subsequence-i/discuss/380325/Solution-in-Python-3-(beats-~90)-(one-line)
+
 class Solution:
     def findLUSlength(self, a: str, b: str) -> int:
         return -1 if a == b else max(len(a), len(b))
