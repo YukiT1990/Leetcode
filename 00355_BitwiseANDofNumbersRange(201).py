43248c1db8f87b8b772ec9a4f29ac54558713a09
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-10-11 09:00:58 +0900

    modify 355

--- 00355_BitwiseANDofNumbersRange(201).py
+++ 00355_BitwiseANDofNumbersRange(201).py
@@ -6,3 +6,14 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         return 0 if  right >= left*2 else functools.reduce(lambda x, y: x & y, range(left, right+1))
+
+"""
+functools.reduce(function, iterable[, initializer])¶
+iterable の要素に対して、iterable を単一の値に短縮するような形で 2 つの引数をもつ function を左から右に累積的に適用します。
+例えば、 reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]) は ((((1+2)+3)+4)+5) を計算します。
+左引数 x は累計の値になり、右引数 y は iterable から取り出した更新値になります。
+オプションの initializer が存在する場合、計算の際に iterable の先頭に置かれます。
+また、 iterable が空の場合には標準の値になります。 initializer が与えられておらず、 
+iterable が単一の要素しか持っていない場合、最初の要素が返されます。
+https://docs.python.org/ja/3/library/functools.html
+"""
