57425b97f26c87de43b56f53b8ca45646fe7dd53
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-10-14 19:04:04 +0900

    modify 363

--- 00363_PerfectSquares(279).py
+++ 00363_PerfectSquares(279).py
@@ -1,5 +1,8 @@
 # 279. Perfect Squares
 
+# Reference
+# https://leetcode.com/problems/perfect-squares/discuss/1006278/Easy-Python-DP-solution-with-explanation-(Dry-Run)
+
 class Solution:
     def numSquares(self, n: int) -> int:
         dp = [i for i in range(n+1)]
